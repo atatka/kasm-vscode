d2a021172cfade916cfa92c38caf93985e283883
atatka <79046234+atatka@users.noreply.github.com>
2023-07-17 11:44:03 +0200

    Dockerfile for building a custom kasm vscode image

---
+++ src/install_vs_code.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -ex
+ARCH=$(arch | sed 's/aarch64/arm64/g' | sed 's/x86_64/x64/g')
+wget -q https://update.code.visualstudio.com/latest/linux-deb-${ARCH}/stable -O vs_code.deb
+apt-get update
+apt-get install -y ./vs_code.deb
+mkdir -p /usr/share/icons/hicolor/apps
+wget -O /usr/share/icons/hicolor/apps/vscode.svg https://kasm-static-content.s3.amazonaws.com/icons/vscode.svg
+sed -i '/Icon=/c\Icon=/usr/share/icons/hicolor/apps/vscode.svg' /usr/share/applications/code.desktop
+sed -i 's#/usr/share/code/code#/usr/share/code/code --no-sandbox##' /usr/share/applications/code.desktop
+cp /usr/share/applications/code.desktop $HOME/Desktop
+chmod +x $HOME/Desktop/code.desktop
+chown 1000:1000 $HOME/Desktop/code.desktop
+rm vs_code.deb
+
+# Conveniences for python development
+apt-get update
+apt-get upgrade
+apt-get install -y htop curl wget nano zsh mc ncdu tmux \
+    build-essential libssl-dev automake autoconf libncurses5-dev \
+    dnsutils net-tools \
+    openssh-client \
+    git \
+    mtr-tiny \
+    gawk \
+    neofetch \
+    rsync \
+    telnet
+
+# Cleanup
+if [ -z ${SKIP_CLEAN+x} ]; then
+  apt-get autoclean
+  rm -rf \
+    /var/lib/apt/lists/* \
+    /var/tmp/*
+fi
